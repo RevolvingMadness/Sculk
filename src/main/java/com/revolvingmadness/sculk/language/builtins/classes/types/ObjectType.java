2db6d196f4c9bd87fe9d0809640988fd29076c36
RevolvingMadness <andyallen216@gmail.com>
2024-02-14 18:52:14 -0600

    Add `Object.equals` default method

--- src/main/java/com/revolvingmadness/sculk/language/builtins/classes/types/ObjectType.java
+++ src/main/java/com/revolvingmadness/sculk/language/builtins/classes/types/ObjectType.java
@@ -3,6 +3,7 @@ package com.revolvingmadness.sculk.language.builtins.classes.types;
 import com.revolvingmadness.sculk.language.builtins.classes.BuiltinClass;
 import com.revolvingmadness.sculk.language.builtins.classes.BuiltinMethod;
 import com.revolvingmadness.sculk.language.builtins.classes.BuiltinType;
+import com.revolvingmadness.sculk.language.builtins.classes.instances.BooleanInstance;
 import com.revolvingmadness.sculk.language.builtins.classes.instances.NullInstance;
 import com.revolvingmadness.sculk.language.builtins.classes.instances.StringInstance;
 import com.revolvingmadness.sculk.language.interpreter.Interpreter;
@@ -18,6 +19,7 @@ public class ObjectType extends BuiltinType {
 
         this.typeVariableScope.declare(List.of(TokenType.CONST), "init", new Init());
         this.typeVariableScope.declare(List.of(TokenType.CONST), "toString", new ToString());
+        this.typeVariableScope.declare(List.of(TokenType.CONST), "equals", new Equals());
     }
 
     private static class Init extends BuiltinMethod {
@@ -35,6 +37,17 @@ public class ObjectType extends BuiltinType {
             this.validate("toString", arguments);
 
             return new StringInstance(this.boundClass.toString());
+        }
+    }
+
+    private static class Equals extends BuiltinMethod {
+        @Override
+        public BuiltinClass call(Interpreter interpreter, List<BuiltinClass> arguments) {
+            this.validate("equals", arguments, List.of(ObjectType.TYPE));
+
+            BuiltinClass object = arguments.get(0);
+
+            return new BooleanInstance(this.boundClass.equals(object));
         }
     }
 }
